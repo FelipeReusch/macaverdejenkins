18f07ff0e22d9731b28ffa5cacdd16e943a793dc
felipe reusch <felipereusch@MacBook-Air-de-felipe.local>
2021-11-18 11:10:59 -0300

    jenkins

---
+++ src/test/java/com/dh/macaverdejenkins/MacaverdejenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.macaverdejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacaverdejenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
